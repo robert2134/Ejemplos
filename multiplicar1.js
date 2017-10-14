48c0c6b795eb2c82b0f3b2f01d8602a351dcd7ca
roberto <roberto.tarazon@gmail.com>
2017-10-14 14:25:15 +0200

    2 diferentes ejem tablas

---
+++ multiplicar1.js
@@ -0,0 +1,9 @@
+// pide un numero
+  var numero=prompt("Introduce un numero entre el 1 y el 100");
+// muestra numero entero
+     alert(parseInt(numero));
+// comrueba que esta dentro limite
+    if (numero >0 && numero < 101){
+        alert ("Observa la tabla");
+      } else
+        alert("vuelve a introducirlo");
---
+++ multiplicar2.js
@@ -0,0 +1,12 @@
+<script>
+// pide un numero
+       var numero=prompt("Introduce un numero entre el 1 y el 100");
+// muestra numero entero
+          alert(parseInt(numero));
+// comrueba que esta dentro limite
+       do{
+         numero > 0;
+         console.log(numero);
+       } while (numero < 101);
+      alert("vuelve a introducirlo");
+</script>
